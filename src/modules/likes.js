19b1a75e7c92f717df84faf675d2a690176e5a26
Rebelzob <karimdavid07@gmail.com>
2023-08-03 22:46:05 +0100

    Linter fixes

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,20 +1,20 @@
 import { getLikes, postLikes } from './apis.js';
 
-const clickLike = (element,textElement) => {
+const clickLike = (element, textElement) => {
   element.addEventListener('click', () => {
     postLikes(element.id).then(((value) => {
       if (value === 'Created' || value === '201') {
-        textElement.innerText = parseInt(textElement.innerText) + 1;
+        textElement.innerText = parseInt(textElement.innerText, 10) + 1;
       }
     }));
   });
-}
+};
 
 const displayLikes = (itemId, elementDisplay) => {
   getLikes().then(((value) => {
-    const itemObject = value.find(item => item.item_id == itemId);
+    const itemObject = value.find((item) => item.item_id === itemId);
     elementDisplay.innerText = itemObject.likes;
   }));
-}
+};
 
 export { clickLike, displayLikes };
